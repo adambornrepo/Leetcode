bcaa7c31f332ca7907ed131b7bcfd6f75fcdd3aa
adamborn <adamd.born@gmail.com>
2023-01-21 15:35:16 +0300

    Plus One

---
+++ src/easy/Problem66.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class Problem66 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] result = new int[digits.length + 1];
+        if (digits[0] == 0) {
+            result[0] = 1;
+        }
+        return result;
+    }
+}
